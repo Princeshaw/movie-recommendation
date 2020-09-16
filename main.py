b03bdbe1d2df3bf09a32c1bad0952a658e49c85e
Princeshaw <prince54shaw@gmail.com>
2020-09-17 03:58:42 +0530

    Used tmdb API

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ import bs4 as bs
 import urllib.request
 import pickle
 import requests
+from tmdbv3api import TMDb
+import json
+from tmdbv3api import Movie
+tmdb_movie = Movie()
+
+file = open("./key.json",'r')
+json_file = json.load(file)
+api_key = json_file['api_key']
+
+tmdb = TMDb()
+tmdb.api_key = api_key
+
 def create_similarity():
     data = pd.read_csv('./clean_dataset/main_data.csv')
     # creating a count matrix
@@ -37,15 +49,34 @@ def recommendation(m):
         return l    
 
 app=Flask(__name__)
+def get_genre(x):
+    genres=[]
+    result = tmdb_movie.search(x)
+    movie_id = result[0].id
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key={}'.format(movie_id,tmdb.api_key))
+    data_json = response.json()
+    poster = data_json['poster_path']
+    if data_json['genres']:
+        genre_str = " " 
+        for i in range(0,len(data_json['genres'])):
+            genres.append(data_json['genres'][i]['name'])
+        return genre_str.join(genres),poster
+    else:
+        return(None,poster)
 @app.route('/',methods=['GET','POST'])
 def home():
     if request.method=='POST':
         movie = request.form['movie']
         print(movie)
+        genre_list = []
+        posters = []
         rmd = recommendation(movie)
-        print(rmd)
+        for m in rmd:
+            g,p = get_genre(m)
+            genre_list.append(g)
+            posters.append(p)
         # Handle POST Request here
-        return render_template('recommendation.html',movie=movie,list_of_movies = rmd)
+        return render_template('recommendation.html',movie=movie,details = zip(rmd,genre_list,posters))
     return render_template('home.html')
 
 if __name__ == '__main__':
